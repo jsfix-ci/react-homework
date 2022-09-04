3fc0e8053d54c0ed9d4558709acff557c6b44955
Nadzeya Bukauniova <bukavneva99@mail.ru>
2022-09-04 18:11:14 +0300

    hw 39

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,24 +1,20 @@
 import React from "react";
 import styles from './posts.module.css';
-import { useState } from "react";
-import {allPosts} from "../Data/Data";
+import { useState, useEffect } from "react";
 
 
-export const Posts = ({value}) => {
-    const favPosts = allPosts.filter(post => post.isFavorite)
-    console.log(favPosts)
+export const Posts = () => {
+    const [allPosts, setAllPosts] = useState([]);
+    
+    useEffect(() => {
+        fetch("https://60f699f318254c00176e0362.mockapi.io/posts")
+        .then((response) => response.json())
+        .then((data) => setAllPosts(data))
+    }, []);
 
+    console.log({allPosts})
 
     return <ul className="list"> 
-    {value===1 ? <>
-        {favPosts.map((item)=>{
-            return (<li className="list_item" key={item.id}>
-            <p className="item__title">{item.title}</p>
-            <img src={item.image}/>
-            <p className="item__date">{item.date}</p>
-            <p className="item__text">{item.text}</p>
-            <a href={item.link} className="button">Read more</a>
-        </li> )})}</> :
         <>
         {allPosts.map((item)=>{
             return (<li className="list_item" key={item.id}>
@@ -29,7 +25,6 @@ export const Posts = ({value}) => {
             <a href={item.link} className="button">Read more</a>
         </li> )})}
         </>
-    }
-</ul>
+        </ul>
 }
 
