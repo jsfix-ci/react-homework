a6e3608980921fa92f3c9fa81da7e10ff98386cc
Denis <d-dashkevich@mail.ru>
2022-09-08 00:26:35 +0300

    homework-40

--- src/Layouts/Layout/Layout.jsx
+++ src/Layouts/Layout/Layout.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header } from '../Header';
 import { Footer } from '../Footer';
+import {ThemeContext} from "../../context/themeContext";
 
 export const Layout = () => {
+    const [darkTheme, setDarkTheme] = useState(false);
+    
     return (
-        <>
+        <ThemeContext.Provider value={[darkTheme, setDarkTheme]}>
             <Header />
             <main style={{ minHeight: '70vh' }}>
                 <Outlet />
             </main>
             <Footer />
-        </>
+        </ThemeContext.Provider>
     );
 };
